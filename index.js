944852ac8b7de4a7f70819d0bc759ee28e9fc058
Farzad YZ <farskid@gmail.com>
2016-11-29 02:46:29 +0330

    1st commit

---
+++ index.js
@@ -0,0 +1,149 @@
+
+var storageWatcher = function() {
+
+  /*
+    Check for equality of two objects
+  */
+  function isEqual(a, b) {
+
+    // Grab a clone of both
+    var _a = clone(a), _b = clone(b)
+
+    // Delete length property of window.localStorage
+    delete _a.length
+    delete _b.length
+
+    // Check for equality by mapping
+    for (var i in _b) {
+      if (!(i in _a) || (_a[i] !== _b[i])) return false
+    }
+
+    return true
+
+  }
+
+  /*
+    Diff of two objects a, b
+  */
+
+  function diff(a, b) {
+    var _a = clone(a), _b = clone(b)
+
+    var _diff = []
+
+    delete _a.length
+    delete _b.length
+
+    if (isEqual(_a, _b)) return false
+
+    for (var i in _b) {
+      if (!(i in _a) || (_a[i] !== _b[i])) {
+        _diff.push('' + i + ': ' + _a[i] + ' ==> ' + _b[i])
+      }
+    }
+
+    return _diff
+  }
+
+  /*
+    Clone Object
+  */
+  function clone(obj) {
+    return JSON.parse(JSON.stringify(obj))
+  }
+
+  /*
+    Merge two objects a, b
+  */
+  function merge(a, b) {
+    for (var key in b) { a[key] = b[key] }
+      return a
+  }
+
+  /*
+    Clone window.localStorage
+  */
+  function cloneStorage() {
+    var _clone = clone(window.localStorage)
+    delete _clone.length
+    return _clone
+  }
+
+  /* variables */
+  var interval, start, stop, isRunning = false, configure, settings, log, lastStorage = cloneStorage()
+
+  // Default Settings
+  settings = {
+    duration: 1500,
+    verbose: true,
+    logType: 'info'
+  }
+
+  /* Logger */
+  log = function(msg) {
+
+    var _child = document.createElement('div')
+
+    // Do nothing in case verbose is off
+    if (!settings.verbose) return false
+    
+    // Support array of multiple messages
+    if (Array.isArray(msg)) msg.map(function(m) {
+      console[settings.logType](m);
+    })
+    // Single message
+    else {
+      console[settings.logType](msg)
+    }
+
+  }
+  
+  /* Configure watcher and change settings */
+  configure = function(setts) {
+    settings = merge(settings, setts)
+    log([`configured watcher to`, settings])
+  }
+
+  /* Start watcher */
+  start = function() {
+
+    if (isRunning) {
+      log('Start request rejetced because the watcher is already running')
+      return false
+    }
+
+    // Set state
+    isRunning = true
+
+    // Assign interval
+    interval = setInterval(function() {
+      if (!isEqual(lastStorage, window.localStorage)) {
+        log(['storage changed', diff(lastStorage, window.localStorage)])
+        lastStorage = cloneStorage()
+      }
+      else {
+        log('No change')
+      }
+    }, settings.duration)
+
+    log('watcher started')
+  }
+
+  /* Stop watcher */
+  stop = function() {
+    isRunning = false
+    clearInterval(interval)
+    log('watcher stopped')
+  }
+
+  /* Public methods and props */
+  return {
+    configure,
+    start,
+    stop,
+    lastStorage
+  }
+
+}
+
+module.exports = storageWatcher
---
+++ readme.md
@@ -0,0 +1,48 @@
+# LocalStorage watcher
+
+Watch changes in browser localStorage
+
+## Installation
+Simply run `npm install localstorage-watcher --save` or clone the repository.
+
+## Usage
+The whole system works on `storageWatcher` global variable.
+
+### Methods and Properties
+* `settings` private property, an object that holds default settings. Default settings are:
+	1. __duration__ defaults to 1500ms or 1.5 seconds
+	2. __logType__ defaults to "info" which logger will use to run `console.info`. Other options are `error`, `log`, `table`, `warn`
+	3. __verbose__ defaults to `true` that will allow logger to log changes, difference of **storageWatcher.lastStorage** and **window.localStorage**. To turn logger off, change this to `false`
+* `configure` public method, this method configures settings.
+* `start` public method, starts the watcher
+* `stop` public method, stops the watcher
+* `lastStorage` public property, holds last storage object before recent change
+
+### Using methods
+```javascript
+  var watcher = storageWatcher;
+
+  // Configure watcher
+  watcher.configue({
+  	verbose: true, // (Bool) Default: true
+  	duration: 3000, // (Integer) Default: 1500
+  	logType: 'warn' // (String) Default: 'info'
+  })
+
+  // Start watcher
+  watcher.start();
+
+  // add to storage to see the watcher's behavior
+  localStorage.setItem('testKey', 'testValue');
+
+  // on next watch cycle, the watcher will log the change and the difference of watcher.lastStorage and window.localStorage
+
+  // Stop watcher
+  watcher.stop()
+
+  // Get the last saved storage
+  watcher.lastStorage
+```
+
+## Example
+In the __example__ folder, open __index.html__ in browser and you will see a logger system similar to your browser console and bunch of useful buttons. 
